e4b3971167b6a8188579056b0b8fff8286f28b12
memizzos <4321nitro1234@gmail.com>
2019-11-21 19:47:23 +0100

    update

--- scripts.js
+++ scripts.js
@@ -24,7 +24,10 @@ const board = {
 	width: Math.floor(width / cellSize),
 	height: Math.floor(height / cellSize),
 	render: ()=>{
-		/* tu trzeba narysować ramkę i zamalować planszę */	
+		ctx.fillStyle="black";
+		ctx.fillRect(0,0,width,height);
+		ctx.fillStyle="white";
+		ctx.fillRect(offsetX,offsetY,cellSize*board.width,cellSize*board.height);
 	}
 }
 
@@ -32,13 +35,15 @@ const snake = {
 	sections:[],
 	direction:'right',
 	addHead:(x,y)=>{
-		/* tu trzeba dodać głowę! */
+		snake.sections = [Section(x,y)].concat(snake.sections)
+		if(snake.sections.length > 1) snake.sections[1].head = false;
 	},
 	removeTail(){
-		/* tu trzeba usunąć ogon */
+		if(snake.sections.length>1) return snake.sections.pop();
+		return false;
 	},
 	render:()=>{
-		/* tu trzeba narysować wszystkie moduły  */
+		snake.sections.forEach(renderSection);
 	}
 }
 function Section(x,y) {
@@ -50,13 +55,24 @@ function Section(x,y) {
 }
 function renderSection(section){
 	/* tu trzeba narysowac moduł */ 
+	ctx.fillStyle = "black";
+	ctx.fillRect(	offsetX+section.x*cellSize,
+					offsetY+section.y*cellSize,
+					cellSize,
+					cellSize);
 }
-function move(directionOverwrite) {
+function move() {
 	let {x,y} = snake.sections[0];
-	const finalDirection = directionOverwrite || snake.direction;
-	// ustalić położenie głowy
+	if(snake.direction === 'right') x++;
+	if(snake.direction === 'left') x--;
+
+	if(snake.direction === 'down') y++;
+	if(snake.direction === 'up') y--;
+	
 	// dodać głowę
+	snake.addHead(x,y);
 	// usunąć ogon
+	snake.removeTail();
 }
 
 const stats = {
@@ -67,12 +83,25 @@ const stats = {
 let timeoutID = 0;
 function step(){
 	// przesunąć wonsza
+	move();
 	// narysować planszę
+	board.render();
 	// narysować wonsza
+	snake.render();
 	// zrobic timeout
+	setTimeout(step,1000*(1/stats.speed))
 }
 
 function init(){
+	let x = Math.floor(board.x/2);
+	let y = Math.floor(board.x/2);
+	snake.addHead(x-4,y);
+	snake.addHead(x-3,y);
+	snake.addHead(x-2,y);
+	snake.addHead(x-1,y);
+	snake.addHead(x,y);
+	board.render();
+	snake.render();
 	// narysować początkowego wonsza
 	// narysować planszę
 	// narysowac wonsza
